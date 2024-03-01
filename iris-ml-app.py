a3e397e606c52c6fc9da121096aad54197c34fe5
Justin <justinliang17@gmail.com>
2024-02-29 22:17:45 -0500

    Update iris-ml-app.py
    Massive Update
    Cleanup to the charts
    Added Dataframes
    + EDA Mean w/ Line Graph

--- iris-ml-app.py
+++ iris-ml-app.py
@@ -15,8 +15,6 @@ st.title("""
 Iris Flower Prediction App
 """)
 
-st.sidebar.header('User Input Parameters')
-
 def user_input_features():
     sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 5.4)
     sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.4)
@@ -31,9 +29,13 @@ def user_input_features():
 
 df = user_input_features()
 
-st.subheader('User Input parameters')
-st.sidebar.write(df)
-st.write(df)
+st.sidebar.layout = 'centered'
+st.subheader('User Input Parameters')
+#st.write(df)
+st.dataframe(df, hide_index = True)
+#st.sidebar.subheader('User Input Parameters')
+#st.sidebar.write(df)
+st.sidebar.dataframe(df, hide_index = True)
 
 iris = datasets.load_iris()
 X = iris.data
@@ -42,19 +44,31 @@ Y = iris.target
 st.subheader('Iris Data Table')
 #st.write(pd.iris)
 df_iris = pd.DataFrame(X, columns = iris.feature_names)
-df_iris['target'] = pd.Series(iris.target)
-st.write(df_iris)
+df_iris['target'] = iris['target']
+#df_iris.head()
+#st.dataframe(df_iris, hide_index = True)
 
-_, ax = plt.subplots()
-scatter = ax.scatter(iris.data[:, 0], iris.data[:, 1], c=iris.target)
-ax.set(xlabel=iris.feature_names[0], ylabel=iris.feature_names[1])
-_ = ax.legend(
-    scatter.legend_elements()[0], iris.target_names, loc="lower right", title="Classes"
-)
+df_iris1 = pd.DataFrame(data=np.c_[iris['data'], iris['target']],
+                  columns= iris['feature_names'] + ['target']).astype({'target': int}) \
+       .assign(species=lambda x: x['target'].map(dict(enumerate(iris['target_names']))))
+st.dataframe(df_iris1, hide_index = True)
 
+st.subheader('EDA - Mean Grouped by Class')
+df_iris2 = df_iris1.drop(['target'], axis = 1 )
+groupby_species_mean = df_iris2.groupby('species').mean()
+st.write(groupby_species_mean)
+st.line_chart(groupby_species_mean.T)
+
+st.subheader('Class labels and Their Corresponding Index #')
+df1 = pd.DataFrame(df_iris1.target.dropna().unique(), columns=['Species #'])
+df2 = pd.DataFrame(df_iris1.species.dropna().unique(), columns=['Species'])
+st.dataframe(pd.concat([df1, df2], axis = 1, ), hide_index = True)
+
+#Training
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.25, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.25, random_state = 42)
 
+#Scaling but this creates a lot of flux in MAE
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
@@ -62,24 +76,25 @@ X_test = sc.transform(X_test)
 
 # Importing the random forest classifier model and fitting
 from sklearn.ensemble import RandomForestClassifier
-classifier = RandomForestClassifier(n_estimators = 100, random_state = 0)
+classifier = RandomForestClassifier(max_depth = 2, max_features = 4, n_estimators = 200, random_state = 42)
 classifier.fit(X_train, y_train)
 
 # Predicting on the test dataset 
 prediction = classifier.predict(df)
 prediction_proba = classifier.predict_proba(df)
 
-st.subheader('Class labels and their corresponding index number')
-st.write(iris.target_names)
-
-st.subheader('Prediction')
-st.write(iris.target_names[prediction])
-
-st.subheader('Prediction Number')
-st.write(prediction)
+st.subheader('Prediction & Prediction Number')
+p1 = pd.DataFrame(iris.target_names[prediction], columns = ['Species'])
+p2 = pd.DataFrame(prediction, columns = ['Species #'])
+st.dataframe(pd.concat([p1, p2], axis = 1), hide_index = True)
+#st.write(iris.target_names[prediction])
+#st.write(prediction)
 
 st.subheader('Prediction Probability')
-st.write(prediction_proba)
+#st.write(prediction_proba)
+st.dataframe(prediction_proba, hide_index = True)
+#pp = pd.DataFrame(prediction_proba, columns = ['Prob 1', 'Prob 2'])
+#st.dataframe(pp, hide_index = True)
 
 y_pred = classifier.predict(X_test)
 
